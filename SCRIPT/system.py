f3ab60e4f6eb603bf4f87de0c345f78be1549078
root <root@localhost.localdomain>
2026-09-15 18:00:00 -0400

    estrutura ha

--- SCRIPT/system.py
+++ SCRIPT/system.py
@@ -1,1 +1,22 @@
+import uuid
 
+
+class Feedback:
+    def __init__(self, usuario, nota, mensagem):
+        self.id = uuid.uuid4()
+        self.usuario = usuario
+        self.nota = nota
+        self.mensagem = mensagem
+
+
+def novo_feedback(usuario, nota, mensagem):
+    feedback = Feedback(usuario, nota, mensagem)
+    return feedback
+
+#essas duas por enquanto só quando tiver o banco funcionando:(
+def buscar_feedback(id_feedback):
+    pass
+
+
+def listar_feedbacks():
+    pass
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
 
+# --- HOME ---
+@app.route('/')
+def home():
+    return render_template('home')
+
+# --- TELA LOGIN ---
+
+# --- TELA CADASTRO ---
+
+
+
+
+if __name__ == '__main__':
+    app.run()
