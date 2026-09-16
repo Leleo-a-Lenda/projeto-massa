5cbaa3bd7c536c1ad33d6856e46217dac40fd4a5
Erick Levi <ericklevioutro@gmail.com>
2026-09-16 14:11:40 -0300

    rotas criadas

--- main.py
+++ main.py
@@ -4,14 +4,22 @@ app = Flask(__name__)
 # --- HOME ---
 @app.route('/')
 def home():
-    return render_template('home')
+    return render_template('homepage.html',)
 
 # --- TELA LOGIN ---
+@app.route('/login')
+def login():
+    return render_template('login.html',)
 
 # --- TELA CADASTRO ---
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html',)
 
-
-
+# --- TELA FEEDBACK ---
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html',)
 
 if __name__ == '__main__':
     app.run()
